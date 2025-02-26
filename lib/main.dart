33cdbff9e3cb5c0968aa8ed74f3615035c94c9c1
bhnm254 <jenavie@nughtclab.com>
2025-02-26 13:10:22 +0000

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,196 +1,122 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:provider/provider.dart';
-import 'package:uuid/uuid.dart';
-import 'dart:convert'; // Add this import
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => BluetoothProvider()),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bluetooth Chat',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Bluetooth Chat'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: DeviceListScreen(),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Provider.of<BluetoothProvider>(context, listen: false).startScan();
-            },
-            child: Text('Scan for Devices'),
-          ),
-        ],
-      ),
-    );
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class DeviceListScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<BluetoothProvider>(
-      builder: (context, provider, child) {
-        return ListView.builder(
-          itemCount: provider.devices.length,
-          itemBuilder: (context, index) {
-            final device = provider.devices[index];
-            return ListTile(
-              title: Text(device.name.isEmpty ? 'Unknown Device' : device.name),
-              subtitle: Text(device.id.toString()),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChatScreen(device: device),
-                  ),
-                );
-              },
-            );
-          },
-        );
-      },
-    );
-  }
-}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-class ChatScreen extends StatefulWidget {
-  final BluetoothDevice device;
-
-  ChatScreen({required this.device});
-
-  @override
-  _ChatScreenState createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController _messageController = TextEditingController();
-  List<String> messages = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _connectToDevice();
-  }
-
-  void _connectToDevice() async {
-    await widget.device.connect();
-    print("Connected to ${widget.device.name}");
-
-    // Listen for incoming messages
-    widget.device.discoverServices().then((services) {
-      for (var service in services) {
-        for (var characteristic in service.characteristics) {
-          if (characteristic.properties.read) {
-            characteristic.read().then((value) {
-              setState(() {
-                messages.add("Received: ${String.fromCharCodes(value)}");
-              });
-            });
-          }
-        }
-      }
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
   }
 
-  void _sendMessage(String message) async {
-    final services = await widget.device.discoverServices();
-    for (var service in services) {
-      for (var characteristic in service.characteristics) {
-        if (characteristic.properties.write) {
-          await characteristic.write(utf8.encode(message)); // Fixed line
-          setState(() {
-            messages.add("You: $message");
-          });
-          _messageController.clear();
-        }
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat with ${widget.device.name}'),
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(messages[index]),
-                );
-              },
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _messageController,
-                    decoration: InputDecoration(hintText: "Type a message"),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: () {
-                    if (_messageController.text.isNotEmpty) {
-                      _sendMessage(_messageController.text);
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-}
-
-class BluetoothProvider with ChangeNotifier {
-  List<BluetoothDevice> devices = [];
-
-  void startScan() {
-    FlutterBluePlus.startScan(timeout: Duration(seconds: 10));
-    FlutterBluePlus.scanResults.listen((results) {
-      for (var result in results) {
-        if (!devices.contains(result.device)) {
-          devices.add(result.device);
-        }
-      }
-      notifyListeners();
-    });
   }
 }
